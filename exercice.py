f83893bad70fa5a02da5f37babe405f0753d2711
Huy Viet Nguyen <90419850+UiVieto@users.noreply.github.com>
2021-10-26 18:47:03 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,10 +3,23 @@
 
 # TODO: Importez vos modules ici
 
+import numpy
 
 
 # TODO: Définissez vos fonction ici
 
+# 1. Volume et masse d'un ellipsoïde
+
+def volume_masse(a, b, c, masse_volumique):
+    
+    # Calcul du volume
+    volume = (4/3) * numpy.pi * a * b * c
+
+    # Calcul de la masse
+
+    masse = masse_volumique * volume
+
+    return (volume, masse)
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
